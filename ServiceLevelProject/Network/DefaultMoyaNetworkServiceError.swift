2742a8bfe3e3c565539717728782f8e7cbe55c1f
Youngminah <bluebelsmudi@gmail.com>
2022-01-22 17:55:34 +0900

    add: moya 네트워크 에러 케이스

---
+++ ServiceLevelProject/Network/DefaultMoyaNetworkServiceError.swift
@@ -0,0 +1,35 @@
+//
+//  MoyaNetworkServiceError.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/01/22.
+//
+
+import Foundation
+
+enum DefaultMoyaNetworkServiceError: Int, Error, CustomStringConvertible {
+
+    case duplicatedError = 201
+    case inValidInputBodyError = 202
+    case inValidFCMTokenError = 401
+    case internalServerError = 500
+    case internalClientError = 501
+    case unknown
+
+    var description: String { self.errorDescription }
+}
+
+extension DefaultMoyaNetworkServiceError {
+
+    var errorDescription: String {
+        switch self {
+        case .duplicatedError: return "201:DUPLICATE_ERROR"
+        case .inValidInputBodyError: return "202:INVALID_INPUT_BODY_ERROR"
+        case .inValidFCMTokenError: return "401:INVALID_FCM_TOKEN_ERROR"
+        case .internalServerError: return "500:INTERNAL_SERVER_ERROR"
+        case .internalClientError: return "501:INTERNAL_CLIENT_ERROR"
+        default: return "UN_KNOWN_ERROR"
+        }
+    }
+}
+
