1f3f46b3a89492f883f975bfc649c186d34352dd
Youngminah <bluebelsmudi@gmail.com>
2022-01-22 14:40:31 +0900

    add: 스탑워치 변환 서비스

---
+++ ServiceLevelProject/Utils/Service/StopWatchConverterService.swift
@@ -0,0 +1,28 @@
+//
+//  StopWatch.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/01/20.
+//
+
+import Foundation
+
+struct StopWatchConverterService {
+
+    var totalSeconds: Int
+    var minutes: Int { return (totalSeconds % 3600) / 60 }
+    var seconds: Int { return totalSeconds % 60 }
+}
+
+extension StopWatchConverterService {
+
+    var simpleTimeString: String {
+        let minutesText = timeText(from: minutes)
+        let secondsText = timeText(from: seconds)
+        return "\(minutesText):\(secondsText)"
+    }
+
+    private func timeText(from number: Int) -> String {
+        return number < 10 ? "0\(number)" : "\(number)"
+    }
+}
