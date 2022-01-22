59d853f82aecf97a74b02163fa1fa87f79a5d707
Youngminah <bluebelsmudi@gmail.com>
2022-01-22 14:43:13 +0900

    update: 네이밍수정

--- ServiceLevelProject/Presentation/Extension/UIFont.swift
+++ ServiceLevelProject/Presentation/Extension/UIFont.swift
@@ -10,39 +10,39 @@ import UIKit.UIFont
 extension UIFont {
     
     static var display1R20: UIFont {
-        return UIFont(name: SLPFont.regular.rawValue, size: 20)!
+        return UIFont(name: AppFont.regular.rawValue, size: 20)!
     }
     static var title1M16: UIFont {
-        return UIFont(name: SLPFont.medium.rawValue, size: 16)!
+        return UIFont(name: AppFont.medium.rawValue, size: 16)!
     }
     static var title2R16: UIFont {
-        return UIFont(name: SLPFont.regular.rawValue, size: 16)!
+        return UIFont(name: AppFont.regular.rawValue, size: 16)!
     }
     static var title3M14: UIFont {
-        return UIFont(name: SLPFont.medium.rawValue, size: 14)!
+        return UIFont(name: AppFont.medium.rawValue, size: 14)!
     }
     static var title4R14: UIFont {
-        return UIFont(name: SLPFont.regular.rawValue, size: 14)!
+        return UIFont(name: AppFont.regular.rawValue, size: 14)!
     }
     static var title5M12: UIFont {
-        return UIFont(name: SLPFont.medium.rawValue, size: 12)!
+        return UIFont(name: AppFont.medium.rawValue, size: 12)!
     }
     static var title5R12: UIFont {
-        return UIFont(name: SLPFont.regular.rawValue, size: 12)!
+        return UIFont(name: AppFont.regular.rawValue, size: 12)!
     }
     static var body1M16: UIFont {
-        return UIFont(name: SLPFont.medium.rawValue, size: 16)!
+        return UIFont(name: AppFont.medium.rawValue, size: 16)!
     }
     static var body2R16: UIFont {
-        return UIFont(name: SLPFont.regular.rawValue, size: 16)!
+        return UIFont(name: AppFont.regular.rawValue, size: 16)!
     }
     static var body3R14: UIFont {
-        return UIFont(name: SLPFont.regular.rawValue, size: 14)!
+        return UIFont(name: AppFont.regular.rawValue, size: 14)!
     }
     static var body4R12: UIFont {
-        return UIFont(name: SLPFont.regular.rawValue, size: 12)!
+        return UIFont(name: AppFont.regular.rawValue, size: 12)!
     }
     static var captionR10: UIFont {
-        return UIFont(name: SLPFont.regular.rawValue, size: 10)!
+        return UIFont(name: AppFont.regular.rawValue, size: 10)!
     }
 }
