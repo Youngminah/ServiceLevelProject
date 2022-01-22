7d9f689e241c66e2e76ee5a8a8822b8c19aeed8d
Youngminah <bluebelsmudi@gmail.com>
2022-01-22 14:49:07 +0900

    feat: 회원가입 네트워크 통신 로직 추가

--- ServiceLevelProject/Presentation/Auth/ViewModel/CertificationViewModel.swift
+++ ServiceLevelProject/Presentation/Auth/ViewModel/CertificationViewModel.swift
@@ -6,12 +6,14 @@
 //
 
 import Foundation
+import Moya
 import RxCocoa
 import RxSwift
 
-final class CertificationViewModel: ViewModelType {
+final class CertificationViewModel: CommonViewModel, ViewModelType {
 
     struct Input {
+        let requestRegisterSignal: Signal<Void>
         let didLimitText: Driver<String>
         let didLimitTime: Signal<Void>
     }
@@ -28,7 +30,7 @@ final class CertificationViewModel: ViewModelType {
     func transform(input: Input) -> Output {
 
         input.didLimitText
-            .map{ $0.count >= 6 ? true : false }
+            .map{ $0.count == 6 ? true : false }
             .distinctUntilChanged()
             .drive(onNext: { [weak self] in
                 guard let self = self else { return }
@@ -37,9 +39,23 @@ final class CertificationViewModel: ViewModelType {
             .disposed(by: disposeBag)
 
         input.didLimitTime
-            .emit (onNext: { [weak self] text in
+            .emit(onNext: { [weak self] text in
                 guard let self = self else { return }
                 self.showToastAction.accept("전화번호 인증 실패")
+            })
+            .disposed(by: disposeBag)
+
+        input.requestRegisterSignal
+            .emit(onNext: { [weak self] text in
+                guard let self = self else { return }
+                self.requestRegister { response in
+                    switch response {
+                    case .success(let code):
+                        print("서버에서 준 코드!! \(code)")
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                }
             })
             .disposed(by: disposeBag)
 
@@ -47,5 +63,23 @@ final class CertificationViewModel: ViewModelType {
             isValidState: isValidState.asDriver(),
             showToastAction: showToastAction.asSignal()
         )
+    }
+}
+
+extension CertificationViewModel {
+
+    func requestRegister(completion: @escaping (Result<Int, Error>) -> Void ) {
+        let userInfo = UserRegisterInfo(
+            phoneNumber: "+821088407593",
+            FCMtoken: UserDefaults.standard.string(forKey: "FCMToken")!,
+            nick: "youngmin",
+            birth: "1994-11-14T09:23:44.054Z",
+            email: "youngminah@gmail.com",
+            gender: 1
+        )
+        let parameters = userInfo.toDictionary
+        provider.request(.register(parameters: parameters)) { result in
+            self.process(result: result, completion: completion)
+        }
     }
 }
