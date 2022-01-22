7db87729c96f45c3656d27f062e498b822e13dd6
Youngminah <bluebelsmudi@gmail.com>
2022-01-22 14:38:44 +0900

    add: api 경로 추가

---
+++ ServiceLevelProject/Utils/Constant/APIConstant.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstant.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/01/21.
+//
+
+import Foundation
+
+struct APIConstant {
+    static let environment: APIEnvironment = .staging
+}
