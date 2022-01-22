45382a76222bc32409bfd5bbda3ea509d6d77b43
Youngminah <bluebelsmudi@gmail.com>
2022-01-22 22:55:28 +0900

    feat: 유저디폴트 키 케이스

---
+++ ServiceLevelProject/Utils/Constant/UserDefaultKey.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaultKey.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/01/22.
+//
+
+import Foundation
+
+enum UserDefaultKey {
+    static let isFirstUser = "isFirstUser"
+    static let isLoggedIn = "isLoggedIn"
+    static let fcmToken = "fcmToken"
+}
