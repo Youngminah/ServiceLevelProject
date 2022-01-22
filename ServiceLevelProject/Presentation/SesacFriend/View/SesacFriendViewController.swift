f922bbe72797c0aa8a22ad4569980288fa8d39b4
Youngminah <bluebelsmudi@gmail.com>
2022-01-23 04:10:18 +0900

    add: 새싹샵, 새싹친구 화면추가

---
+++ ServiceLevelProject/Presentation/SesacFriend/View/SesacFriendViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SesacFriendViewController.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/01/22.
+//
+
+import UIKit
+
+class SesacFriendViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
---
+++ ServiceLevelProject/Presentation/SesacShop/View/SesacShopViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SesacShopViewController.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/01/22.
+//
+
+import UIKit
+
+class SesacShopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+}
