73e3d8a1c0eff8f8be35d88b2985d504955b1c93
Youngminah <bluebelsmudi@gmail.com>
2022-01-22 14:47:12 +0900

    add: 회원가입 유저 인포 바디

---
+++ ServiceLevelProject/Domain/UserRegisterInfo.swift
@@ -0,0 +1,29 @@
+//
+//  UserRegisterInfo.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/01/22.
+//
+
+import Foundation
+
+struct UserRegisterInfo: Codable {
+    let phoneNumber: String
+    let FCMtoken: String
+    let nick: String
+    let birth: String
+    let email: String
+    let gender: Int
+
+    var toDictionary: [String: Any] {
+        let dict: [String: Any]  = [
+            "phoneNumber": phoneNumber,
+            "FCMtoken": FCMtoken,
+            "nick": nick,
+            "birth": birth,
+            "email": email,
+            "gender": gender,
+        ]
+        return dict
+    }
+}
