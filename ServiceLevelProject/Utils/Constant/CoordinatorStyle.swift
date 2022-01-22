70f31c184146bfc274656943a8e7f19fd5323977
Youngminah <bluebelsmudi@gmail.com>
2022-01-22 22:55:12 +0900

    feat: 코디네이터 스타일 케이스

---
+++ ServiceLevelProject/Utils/Constant/CoordinatorStyle.swift
@@ -0,0 +1,13 @@
+//
+//  CoordinatorType.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/01/22.
+//
+
+import Foundation
+
+enum CoordinatorStyle {
+    case app, onBoarding, login, certification
+    case nickname, birth, email, gender
+}
