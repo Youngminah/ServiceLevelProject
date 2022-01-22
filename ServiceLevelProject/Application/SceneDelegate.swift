b51e814458cf5a8eaeca23cd711bc1e08a04d679
Youngminah <bluebelsmudi@gmail.com>
2022-01-22 22:50:11 +0900

    update: 코디네이터 이용한 첫화면 띄우기

--- ServiceLevelProject/Application/SceneDelegate.swift
+++ ServiceLevelProject/Application/SceneDelegate.swift
@@ -10,13 +10,17 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var coordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         //let vc = CertificationViewController(viewModel: CertificationViewModel(), verifyID: "dd")
-        let vc = LoginViewController(viewModel: LoginViewModel())
-        let nav = UINavigationController(rootViewController: vc)
+//        let vc = LoginViewController(viewModel: LoginViewModel())
+//        let nav = UINavigationController(rootViewController: vc)
+        let nav = UINavigationController()
+        coordinator = AppCoordinator(nav)
+        coordinator?.start()
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
     }
