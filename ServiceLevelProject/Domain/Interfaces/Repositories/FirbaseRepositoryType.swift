5581873bf7bbf5dcbe830406c871767b7ecfa64a
Youngminah <bluebelsmudi@gmail.com>
2022-01-24 09:21:43 +0900

    feat: 파이어베이스 레파지토리 프로토콜

---
+++ ServiceLevelProject/Domain/Interfaces/Repositories/FirbaseRepositoryType.swift
@@ -0,0 +1,26 @@
+//
+//  FirbaseRepositoryType.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/01/24.
+//
+
+import Foundation
+
+protocol FirbaseRepositoryType: AnyObject {
+
+    func verifyPhoneNumber(
+        phoneNumber: String,
+        completion: @escaping (Result<String, FirbaseNetworkServiceError>) -> Void
+    )
+
+    func signIn(
+        verifyID: String,
+        certificationNumber: String,
+        completion: @escaping (Result<Void, FirbaseNetworkServiceError>) -> Void
+    )
+
+    func requestIdtoken(
+        completion: @escaping (Result<String, FirbaseNetworkServiceError>) -> Void
+    )
+}
