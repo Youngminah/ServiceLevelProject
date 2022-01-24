965390846ceba048074e5696a8db1aeeff7e5f58
Youngminah <bluebelsmudi@gmail.com>
2022-01-24 09:20:54 +0900

    feat: 로그인 유즈케이스

---
+++ ServiceLevelProject/Domain/UseCase/LoginUseCase.swift
@@ -0,0 +1,38 @@
+//
+//  LoginUseCase.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/01/23.
+//
+
+import Foundation
+import RxSwift
+
+final class LoginUseCase {
+
+    private let userRepository: UserRepositoryType
+    private let fireBaseRepository: FirbaseRepositoryType
+
+    var verifyIDSuccessSignal = PublishSubject<String>()
+    var verifyIDFailSignal = PublishSubject<FirbaseNetworkServiceError>()
+
+    init(
+        userRepository: UserRepositoryType,
+        fireBaseRepository: FirbaseRepositoryType
+    ) {
+        self.userRepository = userRepository
+        self.fireBaseRepository = fireBaseRepository
+    }
+
+    func verifyPhoneNumber(phoneNumber: String) {
+        self.fireBaseRepository.verifyPhoneNumber(phoneNumber: phoneNumber) { [weak self] response in
+            guard let self = self else { return }
+            switch response {
+            case .success(let verifyID):
+                self.verifyIDSuccessSignal.onNext(verifyID)
+            case .failure(let error):
+                self.verifyIDFailSignal.onNext(error)
+            }
+        }
+    }
+}
