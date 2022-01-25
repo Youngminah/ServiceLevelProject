f59df6817cd8a67e88c2b231cdf1512793e6024d
Youngminah <bluebelsmudi@gmail.com>
2022-01-25 22:22:54 +0900

    feat: 닉네임 뷰 모델

--- ServiceLevelProject/Presentation/RegisterScene/ViewModel/NickNameViewModel.swift
+++ ServiceLevelProject/Presentation/RegisterScene/ViewModel/NickNameViewModel.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import Moya
 import RxCocoa
 import RxSwift
 
@@ -15,11 +14,17 @@ final class NickNameViewModel: ViewModelType {
     private weak var coordinator: NickNameCoordinator?
 
     struct Input {
-        let signInFirebaseSignal: Signal<String>
+        let didTextChange: Signal<String>
+        let didNextButtonTap: Signal<String>
     }
     struct Output {
+        let isValidState: Driver<Bool>
+        let showToastAction: Signal<String>
     }
     var disposeBag = DisposeBag()
+
+    private let isValidState = BehaviorRelay<Bool>(value: false)
+    private let showToastAction = PublishRelay<String>()
 
     init(coordinator: NickNameCoordinator?) {
         self.coordinator = coordinator
@@ -27,9 +32,43 @@ final class NickNameViewModel: ViewModelType {
 
     func transform(input: Input) -> Output {
 
+        input.didTextChange
+            .map(validationText)
+            .emit(onNext: { [weak self] isValid in
+                guard let self = self else { return }
+                self.isValidState.accept(isValid)
+            })
+            .disposed(by: disposeBag)
 
+        input.didNextButtonTap
+            .emit(onNext: { [weak self] text in
+                guard let self = self else { return }
+                if self.validationText(text: text) {
+                    self.saveNickName(nickName: text)
+                    self.coordinator?.connectBirthCoordinator()
+                } else {
+                    self.showToastAction.accept("닉네임은 1자 이상 10자 이내로 부탁드려요.")
+                }
+            })
+            .disposed(by: disposeBag)
 
         return Output(
+            isValidState: isValidState.asDriver(),
+            showToastAction: showToastAction.asSignal()
         )
+    }
+}
+
+extension NickNameViewModel {
+
+    private func validationText(text: String) -> Bool {
+        if text.count >= 1 && text.count <= 10 {
+            return true
+        }
+        return false
+    }
+
+    private func saveNickName(nickName: String) {
+        UserDefaults.standard.set(nickName, forKey: UserDefaultKeyCase.nickName)
     }
 }
