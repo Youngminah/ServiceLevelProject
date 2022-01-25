092021cf9ee209f26fd1d98844cbe4c527b230ce
Youngminah <bluebelsmudi@gmail.com>
2022-01-25 22:22:05 +0900

    feat: 유저 저장 케이스화

--- ServiceLevelProject/Utils/Constant/UserDefaultKeyCase.swift
+++ ServiceLevelProject/Utils/Constant/UserDefaultKeyCase.swift
@@ -11,5 +11,10 @@ enum UserDefaultKeyCase {
     static let isFirstUser = "isFirstUser"
     static let isLoggedIn = "isLoggedIn"
     static let fcmToken = "fcmToken"
+    static let phoneNumber = "phoneNumber"
     static let idToken = "idToken"
+    static let nickName = "nickName"
+    static let birth = "birth"
+    static let email = "email"
+    static let gender = "gender"
 }
