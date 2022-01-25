0265bf657b06ac9e477e05915a64375a31fe6178
Youngminah <bluebelsmudi@gmail.com>
2022-01-25 22:24:15 +0900

    feat: 닉네임 뷰컨 인풋 아웃풋 구조화

--- ServiceLevelProject/Presentation/RegisterScene/View/NicknameViewController.swift
+++ ServiceLevelProject/Presentation/RegisterScene/View/NicknameViewController.swift
@@ -6,23 +6,63 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+import Toast
 
 final class NicknameViewController: UIViewController {
 
-    private let descriptionLabel = DescriptionLabel(title: "닉네임을 입력해 주세요")
+    private let descriptionLabel = DefaultLabel(title: "닉네임을 입력해 주세요", font: .display1R20)
     private let nicknameTextField = AuthTextField(placeHolder: "10자 이내로 입력")
     private let nextButton = DefaultFillButton(title: "다음")
 
+    private lazy var input = NickNameViewModel.Input(
+        didTextChange: nicknameTextField.rx.text.orEmpty.asSignal(onErrorJustReturn: ""),
+        didNextButtonTap: nextButton.rx.tap
+            .map { [unowned self] in
+                return self.nicknameTextField.text!
+            }
+            .asSignal(onErrorJustReturn: "")
+    )
+    private lazy var output = viewModel.transform(input: input)
+    private let disposdBag = DisposeBag()
+    private let didTextChange = PublishRelay<String>()
+    private let didNextButtonTap = PublishRelay<String>()
+    private var viewModel: NickNameViewModel
+
+    init(viewModel: NickNameViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("NickNameVC fatal error")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setViews()
         setConstraints()
         setConfigurations()
+        bind()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         nicknameTextField.setBorderLine()
+    }
+
+    private func bind() {
+        output.showToastAction
+            .emit(onNext: { [unowned self] text in
+                self.makeToastStyle()
+                self.view.makeToast(text, position: .top)
+            })
+            .disposed(by: disposdBag)
+
+        output.isValidState
+            .drive(nextButton.rx.isValid)
+            .disposed(by: disposdBag)
     }
 
     private func setViews() {
@@ -52,6 +92,12 @@ final class NicknameViewController: UIViewController {
     }
 
     private func setConfigurations() {
+        title = ""
         view.backgroundColor = .white
+        nicknameTextField.becomeFirstResponder()
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
