c599ae29daddf5d598eaee740b99443edc3831cb
Youngminah <bluebelsmudi@gmail.com>
2022-01-25 22:27:29 +0900

    feat: 이메일 유효성 검증 로직 추가

--- ServiceLevelProject/Presentation/Extension/String.swift
+++ ServiceLevelProject/Presentation/Extension/String.swift
@@ -43,4 +43,11 @@ extension String {
         let isValid = predicate.evaluate(with: self)
         return isValid ? true : false
     }
+
+    func isValidEmail() -> Bool {
+        let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let predicate = NSPredicate(format:"SELF MATCHES %@", regex)
+        let isValid = predicate.evaluate(with: self)
+        return isValid
+    }
 }
