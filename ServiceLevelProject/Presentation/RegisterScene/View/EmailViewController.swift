8aff29837dfe91b62140e34cabea746667492a72
Youngminah <bluebelsmudi@gmail.com>
2022-01-25 22:24:56 +0900

    feat: 이메일 뷰 컨트롤러

--- ServiceLevelProject/Presentation/RegisterScene/View/EmailViewController.swift
+++ ServiceLevelProject/Presentation/RegisterScene/View/EmailViewController.swift
@@ -6,24 +6,64 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+import Toast
+import SnapKit
 
 final class EmailViewController: UIViewController {
 
-    private let descriptionLabel = DescriptionLabel(title: "이메일을 입력해 주세요")
-    private let detailLabel = DetailLabel(title: "휴대폰 번호 변경 시 인증을 위해 사용해요")
+    private let descriptionLabel = DefaultLabel(title: "이메일을 입력해 주세요",
+                                                font: .display1R20)
+    private let detailLabel = DefaultLabel(title: "휴대폰 번호 변경 시 인증을 위해 사용해요",
+                                           font: .title2R16 ,
+                                           textColor: .gray7)
     private let emailTextField = AuthTextField(placeHolder: "SeSAC@email.com")
     private let nextButton = DefaultFillButton(title: "다음")
+
+    private lazy var input = EmailViewModel.Input(
+        didTextChange: emailTextField.rx.text.orEmpty.asSignal(onErrorJustReturn: ""),
+        didNextButtonTap: nextButton.rx.tap.withLatestFrom(emailTextField.rx.text.orEmpty)
+            .asSignal(onErrorJustReturn: "")
+    )
+    private lazy var output = viewModel.transform(input: input)
+    private let disposdBag = DisposeBag()
+
+    private var viewModel: EmailViewModel
+
+    init(viewModel: EmailViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("BirthViewController: fatal error")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setViews()
         setConstraints()
         setConfigurations()
+        bind()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         emailTextField.setBorderLine()
+    }
+
+    private func bind() {
+        output.showToastAction
+            .emit(onNext: { [unowned self] text in
+                self.makeToastStyle()
+                self.view.makeToast(text, position: .top)
+            })
+            .disposed(by: disposdBag)
+
+        output.isValid
+            .drive(nextButton.rx.isValid)
+            .disposed(by: disposdBag)
     }
 
     private func setViews() {
@@ -59,6 +99,12 @@ final class EmailViewController: UIViewController {
     }
 
     private func setConfigurations() {
+        title = ""
         view.backgroundColor = .white
+        emailTextField.becomeFirstResponder()
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
