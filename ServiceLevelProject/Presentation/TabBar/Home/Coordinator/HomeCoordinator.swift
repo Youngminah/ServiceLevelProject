8ea555b6794d8775916760e4f6f6bbcddfadd3d4
Youngminah <bluebelsmudi@gmail.com>
2022-01-28 01:37:25 +0900

    update: 코디네이터 종료 델리게이트

--- ServiceLevelProject/Presentation/TabBar/Home/Coordinator/HomeCoordinator.swift
+++ ServiceLevelProject/Presentation/TabBar/Home/Coordinator/HomeCoordinator.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class HomeCoordinator: Coordinator {
 
+    weak var delegate: CoordinatorDelegate?
     var childCoordinators = [Coordinator]()
     var navigationController: UINavigationController
     var type: CoordinatorStyleCase = .home
