7d72a87794623f855f6c5eb71ba6e64f938507bc
Youngminah <bluebelsmudi@gmail.com>
2022-01-31 04:03:07 +0900

    update: 위치조정

--- ServiceLevelProject/Presentation/Common/CustomView/SesacCardView/SesacPreviewView.swift
+++ ServiceLevelProject/Presentation/Common/CustomView/SesacCardView/SesacPreviewView.swift
@@ -15,6 +15,7 @@ final class SesacPreviewView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setConstraints()
+        setConfigurations()
     }
 
     required init(coder: NSCoder) {
@@ -25,9 +26,9 @@ final class SesacPreviewView: UIView {
         addSubview(toggleButton)
         addSubview(nickNameLabel)
         toggleButton.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(16)
-            make.bottom.right.equalToSuperview().offset(-16)
-            make.width.equalTo(toggleButton.snp.width)
+            make.top.equalToSuperview().offset(10)
+            make.bottom.right.equalToSuperview().offset(-10)
+            make.width.equalTo(toggleButton.snp.height)
         }
         nickNameLabel.snp.makeConstraints { make in
             make.left.top.equalToSuperview().offset(16)
@@ -37,7 +38,21 @@ final class SesacPreviewView: UIView {
     }
 
     private func setConfigurations() {
+        nickNameLabel.text = UserDefaults.standard.string(forKey: UserDefaultKeyCase.nickName)
+        nickNameLabel.textAlignment = .left
         toggleButton.setImage(UIImage(systemName: "chevron.down"), for: .normal)
         toggleButton.tintColor = .gray5
+    }
+
+    func addTarget(target: Any?, action: Selector, event: UIControl.Event) {
+        toggleButton.addTarget(target, action: action, for: event)
+    }
+
+    func setToggleButtonImage(isToggle: Bool) {
+        if isToggle {
+            toggleButton.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+        } else {
+            toggleButton.setImage(UIImage(systemName: "chevron.up"), for: .normal)
+        }
     }
 }
