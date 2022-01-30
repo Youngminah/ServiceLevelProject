4d93c03a0f1bacf0bb2d04b2954c18d2617492b9
Youngminah <bluebelsmudi@gmail.com>
2022-01-31 04:03:44 +0900

    feat: 커스텀 알랏

---
+++ ServiceLevelProject/Presentation/Common/CustomView/AlertView.swift
@@ -0,0 +1,183 @@
+//
+//  AlertView.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/01/31.
+//
+
+import UIKit
+import SnapKit
+
+enum AlertStyle {
+    case confirmAndCancel
+    case confirm
+}
+
+final class AlertView: UIView {
+
+    private let alertView: UIView = {
+       let alert = UIView()
+        alert.backgroundColor = .white
+        alert.layer.masksToBounds = true
+        alert.layer.cornerRadius = 11
+        return alert
+    }()
+
+    private let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.textAlignment = .center
+        titleLabel.numberOfLines = 2
+        titleLabel.font = .body1M16
+        titleLabel.tintColor = .black
+        return titleLabel
+    }()
+
+    private let messageLabel: UILabel = {
+        let messageLabel = UILabel()
+        messageLabel.textAlignment = .center
+        messageLabel.numberOfLines = 2
+        messageLabel.font = .title4R14
+        messageLabel.tintColor = .black
+        return messageLabel
+    }()
+
+    private let confirmButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("확인", for: .normal)
+        button.titleLabel?.font = .body3R14
+        button.backgroundColor = .green
+        button.setTitleColor(.white, for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 11
+        return button
+    }()
+
+    private let cancelButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("취소", for: .normal)
+        button.titleLabel?.font = .body3R14
+        button.backgroundColor = .gray2
+        button.setTitleColor(.label, for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 11
+        return button
+    }()
+
+    private var title: String?
+    private var message: String?
+    private var completion: (() -> Void)?
+    private var buttonStyle: AlertStyle = .confirm
+
+    override init(frame: CGRect){
+        super.init(frame: frame)
+
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    convenience init(
+        title: String,
+        message: String? = nil,
+        completion: (() -> Void)?
+    ) {
+        self.init(frame: CGRect.zero)
+        self.title = title
+        self.message = message
+        self.completion = completion
+    }
+
+    convenience init(
+        title: String,
+        message: String? = nil,
+        buttonStyle: AlertStyle,
+        okCompletion: (() -> Void)?
+    ) {
+        self.init(frame: CGRect.zero)
+        self.title = title
+        self.message = message
+        self.buttonStyle = buttonStyle
+        self.completion = okCompletion
+    }
+
+    func showAlert(in superView: UIWindow) {
+        self.setAttributes()
+        self.setConstraints(in: superView)
+        self.setAnimation()
+    }
+
+    private func setAttributes() {
+        backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        titleLabel.text = title
+        messageLabel.text = message
+        confirmButton.addTarget(self, action: #selector(confirmAction), for: .touchUpInside)
+        cancelButton.addTarget(self, action: #selector(cancelAction), for: .touchUpInside)
+    }
+
+    @objc private func confirmAction() {
+        removeAnimation()
+        completion?()
+    }
+
+    @objc private func cancelAction() {
+        removeAnimation()
+    }
+
+    private func setConstraints(in superView: UIWindow) {
+        superView.addSubview(self)
+        frame = superView.frame
+        addSubview(alertView)
+        alertView.addSubview(titleLabel)
+        alertView.addSubview(messageLabel)
+        alertView.addSubview(cancelButton)
+        alertView.addSubview(confirmButton)
+
+        alertView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview().offset(16)
+            make.right.equalToSuperview().offset(-16)
+            make.height.equalTo(alertView.snp.width).multipliedBy(156.0 / 344)
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.left.right.equalToSuperview()
+            make.height.equalTo(30)
+        }
+        messageLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.left.right.equalToSuperview()
+            make.height.equalTo(30)
+        }
+        cancelButton.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(16)
+            make.bottom.equalToSuperview().offset(-16)
+            make.width.equalToSuperview().offset(-20).dividedBy(2.0)
+            make.height.equalTo(cancelButton.snp.width).multipliedBy(48 / 152.0)
+        }
+        confirmButton.snp.makeConstraints { make in
+            make.right.bottom.equalToSuperview().offset(-16)
+            make.width.equalToSuperview().offset(-20).dividedBy(2.0)
+            make.height.equalTo(confirmButton.snp.width).multipliedBy(48 / 152.0)
+        }
+    }
+
+    private func setAnimation() {
+        alertView.alpha = 0
+        UIView.animate(withDuration: 0.2, animations: { [weak self] in
+            guard let strongself = self else { return }
+            strongself.alertView.alpha = 1
+        })
+    }
+
+    private func removeAnimation() {
+        UIView.animate(withDuration: 0.2, animations: { [weak self] in
+            guard let self = self else { return  }
+            self.backgroundColor = .clear
+            self.alertView.alpha = 0
+        }, completion: { [weak self] _ in
+            guard let self = self else { return }
+            self.removeFromSuperview()
+        })
+    }
+}
