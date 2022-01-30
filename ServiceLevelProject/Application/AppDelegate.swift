d1868ffc53f778ad4381df1c4fe634abf8de5c57
Youngminah <bluebelsmudi@gmail.com>
2022-01-31 04:01:54 +0900

    update: 공백 추가

--- ServiceLevelProject/Application/AppDelegate.swift
+++ ServiceLevelProject/Application/AppDelegate.swift
@@ -73,7 +73,7 @@ extension AppDelegate: MessagingDelegate {
     }
 
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        let dataDict:[String: String] = ["token": fcmToken ?? ""]
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
         NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
         // TODO: If necessary send token to application server.
         // Note: This callback is fired at each app startup and whenever a new token is generated.
