ad73e3abcd63fce0140636c2aaebed224c003432
Youngminah <bluebelsmudi@gmail.com>
2022-02-03 14:02:04 +0900

    feat: 유저 업데이트 DTO

---
+++ ServiceLevelProject/Data/DataMapping/UserUpdateInfoRequestDTO.swift
@@ -0,0 +1,36 @@
+//
+//  User.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/02/02.
+//
+
+import Foundation
+
+struct UserUpdateInfoRequestDTO: Codable {
+
+    var toDictionary: [String: Any] {
+        let dict: [String: Any] = [
+            "searchable": searchable,
+            "ageMin": ageMin,
+            "ageMax": ageMax,
+            "gender": gender,
+            "hobby": hobby,
+        ]
+        return dict
+    }
+
+    let searchable: Int
+    let ageMin: Int
+    let ageMax: Int
+    let gender: Int
+    let hobby: String
+
+    init(userUpdateInfo: UserUpdateInfo) {
+        self.searchable = userUpdateInfo.searchable ? 1 : 0
+        self.ageMin = userUpdateInfo.ageMin
+        self.ageMax = userUpdateInfo.ageMax
+        self.gender = userUpdateInfo.gender
+        self.hobby = userUpdateInfo.hobby
+    }
+}
