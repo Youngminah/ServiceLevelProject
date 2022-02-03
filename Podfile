95e58d9230c39e871945d2aa9fb848adad81f13d
Youngminah <bluebelsmudi@gmail.com>
2022-02-03 20:10:27 +0900

    feat: 네이버 맵 sdk

---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '14.0'
+
+target 'ServiceLevelProject' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+   pod 'NMapsMap'
+
+end
--- ServiceLevelProject/Presentation/TabBar/Home/View/HomeViewController.swift
+++ ServiceLevelProject/Presentation/TabBar/Home/View/HomeViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import NMapsMap
 
 final class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        let naverMapView = NMFNaverMapView(frame: view.frame)
+        view.addSubview(naverMapView)
     }
 }
