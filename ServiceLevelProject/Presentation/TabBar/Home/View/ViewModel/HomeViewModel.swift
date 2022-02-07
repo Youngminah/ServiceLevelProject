bb0c9268d66324900782f435ffe910d066ac6a6f
Youngminah <bluebelsmudi@gmail.com>
2022-02-07 21:57:58 +0900

    feat: 주변 새싹 정보 가져오기 뷰모델

--- ServiceLevelProject/Presentation/TabBar/Home/View/ViewModel/HomeViewModel.swift
+++ ServiceLevelProject/Presentation/TabBar/Home/View/ViewModel/HomeViewModel.swift
@@ -18,17 +18,21 @@ final class HomeViewModel: ViewModelType {
 
     struct Input {
         let myLocationButtonTap: Signal<Void>
+        let isAutorizedLocation: Signal<Bool>
+        let requestOnqueueInfo: Signal<Coordinate>
     }
     struct Output {
+        let confirmAuthorizedLocation: Signal<Void>
         let updateLocationAction: Signal<Void>
-        let showToastAction: Signal<String>
-        let indicatorAction: Driver<Bool>
+        let unAutorizedLocationAlert: Signal<(String, String)>
+        let onqueueList: Signal<[SesacDB]>
     }
     var disposeBag = DisposeBag()
 
+    private let confirmAuthorizedLocation = PublishRelay<Void>()
     private let updateLocationAction = PublishRelay<Void>()
-    private let showToastAction = PublishRelay<String>()
-    private let indicatorAction = BehaviorRelay<Bool>(value: false)
+    private let unAutorizedLocationAlert = PublishRelay<(String, String)>()
+    private let onqueueList = PublishRelay<[SesacDB]>()
 
     init(coordinator: HomeCoordinator?, homeUseCase: HomeUseCase) {
         self.coordinator = coordinator
@@ -40,14 +44,48 @@ final class HomeViewModel: ViewModelType {
         input.myLocationButtonTap
             .throttle(.seconds(1), latest: false)
             .emit(onNext: { [weak self] _ in
-                self?.updateLocationAction.accept(())
+                self?.confirmAuthorizedLocation.accept(())
+            })
+            .disposed(by: disposeBag)
+
+        input.isAutorizedLocation
+            .emit(onNext: { [weak self] isEnable in
+                guard let self = self else { return }
+                if isEnable {
+                    self.updateLocationAction.accept(())
+                } else {
+                    self.unAutorizedLocationAlert.accept(("위치 서비스 사용 불가", "아이폰 설정으로 이동합니다."))
+                }
+            })
+            .disposed(by: disposeBag)
+
+        input.requestOnqueueInfo
+            .skip(1)
+            .throttle(.seconds(1), latest: false)
+            .emit(onNext: { [weak self] coordinate in
+                self?.requestOnqueue(coordinate: coordinate)
+            })
+            .disposed(by: disposeBag)
+
+        homeUseCase.successOnqueueSignal.asSignal()
+            .emit(onNext: { [weak self] info in
+                let list = info.fromSesacDB + info.fromSesacDBRequested
+                self?.onqueueList.accept(list)
             })
             .disposed(by: disposeBag)
 
         return Output(
+            confirmAuthorizedLocation: confirmAuthorizedLocation.asSignal(),
             updateLocationAction: updateLocationAction.asSignal(),
-            showToastAction: showToastAction.asSignal(),
-            indicatorAction: indicatorAction.asDriver()
+            unAutorizedLocationAlert: unAutorizedLocationAlert.asSignal(),
+            onqueueList: onqueueList.asSignal()
         )
+    }
+}
+
+extension HomeViewModel {
+
+    private func requestOnqueue(coordinate: Coordinate) {
+        self.homeUseCase.requestOnqueue(coordinate: coordinate)
     }
 }
