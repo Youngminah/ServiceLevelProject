2f6c538c96dd0f2da278d243634d727a76bdfa5a
Youngminah <bluebelsmudi@gmail.com>
2022-02-07 21:56:27 +0900

    feat: 새싹 백그라운드 케이스화

---
+++ ServiceLevelProject/Utils/Constant/SesacBackgroundCase.swift
@@ -0,0 +1,26 @@
+//
+//  SesacBackgroundCase.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/02/07.
+//
+
+import UIKit
+
+enum SesacBackgroundCase: Int, CaseIterable {
+    case background0 = 0
+
+    init(value: Int) {
+        switch value {
+        case 0: self = .background0
+        default: self = .background0
+        }
+    }
+
+    var image: UIImage {
+        switch self {
+        case .background0:
+            return Asset.defaultBackground.image
+        }
+    }
+}
