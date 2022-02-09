ca0247b9208afd5c41182d7704312df83e7d21db
Youngminah <bluebelsmudi@gmail.com>
2022-02-09 19:22:22 +0900

    feat: 홈찾기 vc

--- ServiceLevelProject/Presentation/TabBar/Home/View/View/HomeSearchViewController.swift
+++ ServiceLevelProject/Presentation/TabBar/Home/View/View/HomeSearchViewController.swift
@@ -8,20 +8,55 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import RxDataSources
 import RxKeyboard
 import SnapKit
 
 final class HomeSearchViewController: UIViewController {
 
     private let searchBar = UISearchBar()
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
     let searchSesacButton = DefaultButton(title: "새싹 찾기")
 
-    private let disposedBag = DisposeBag()
+    private lazy var input = HomeSearchViewModel.Input(
+        viewDidLoad: self.rx.viewWillAppear.asSignal()
+    )
+    private lazy var output = viewModel.transform(input: input)
+    private let viewModel: HomeSearchViewModel
+    private let disposeBag = DisposeBag()
+
+    private lazy var dataSource = RxCollectionViewSectionedReloadDataSource<HobbySectionModel> { dataSource, collectionView ,indexPath ,item in
+        print("dataSourcedataSourcedataSourcedataSource")
+        switch item {
+        case .near(let hobby):
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NearHobbyCell.identifier, for: indexPath) as! NearHobbyCell
+            print(hobby)
+            cell.updateUI(hobbyInfo: hobby)
+            return cell
+        case .selected(let hobby):
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SelectedHobbyCell.identifier, for: indexPath) as! SelectedHobbyCell
+            print(hobby)
+            cell.updateUI(hobbyInfo: hobby)
+            return cell
+        }
+    }
+
+    init(viewModel: HomeSearchViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("HomeSearchViewController: fatal error")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        bind()
+        collectionView.register(NearHobbyCell.self, forCellWithReuseIdentifier: NearHobbyCell.identifier)
+        collectionView.register(SelectedHobbyCell.self, forCellWithReuseIdentifier: SelectedHobbyCell.identifier)
+        collectionView.rx.setDelegate(self).disposed(by: disposeBag)
         bindUI()
+        bind()
         setViews()
         setConstraints()
         setConfigurations()
@@ -33,22 +68,32 @@ final class HomeSearchViewController: UIViewController {
     }
 
     private func bind() {
+        print("bind")
+        output.hobbyItems
+            .asDriver(onErrorJustReturn: [])
+            .drive(collectionView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+        viewModel.temp()
     }
 
     private func bindUI() {
         RxKeyboard.instance.visibleHeight
             .drive(rx.keyboardHeightChanged)
-        .disposed(by: disposedBag)
+            .disposed(by: disposeBag)
     }
 
     private func setViews() {
         navigationController?.navigationBar.topItem?.title = ""
         searchBar.sizeToFit()
         navigationItem.titleView = searchBar
+        view.addSubview(collectionView)
         view.addSubview(searchSesacButton)
     }
 
     private func setConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
         searchSesacButton.snp.makeConstraints { make in
             make.right.equalToSuperview().offset(-16)
             make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-16)
@@ -64,29 +109,40 @@ final class HomeSearchViewController: UIViewController {
     }
 }
 
+extension HomeSearchViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 40)
+    }
+}
+
 // MARK: - keyboard
 extension HomeSearchViewController {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        self.view.endEditing(true)
         self.searchBar.resignFirstResponder()
     }
 
     func raiseKeyboardWithButton(keyboardChangedHeight: CGFloat, button: DefaultButton) {
         DispatchQueue.main.async { [weak self] in
             guard let self = self else { return }
-            UIView.animate(withDuration: 0.23, animations: {
-                var bottomOffset = self.view.safeAreaInsets.bottom - keyboardChangedHeight
-                var (offset, radius): (Int, CGFloat) = (0, 0)
-                if keyboardChangedHeight == 0 {
-                    (offset, radius) = (16, 8)
-                    bottomOffset = -16
-                }
-                button.snp.updateConstraints { make in
-                    make.right.equalToSuperview().offset(-offset)
-                    make.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(bottomOffset)
-                    make.left.equalToSuperview().offset(offset)
-                }
-                button.layer.cornerRadius = radius
+            var bottomOffset = self.view.safeAreaInsets.bottom - keyboardChangedHeight
+            var (offset, radius): (Int, CGFloat) = (0, 0)
+            if keyboardChangedHeight == 0 {
+                (offset, radius) = (16, 8)
+                bottomOffset = -16
+            }
+            button.snp.updateConstraints { make in
+                make.right.equalToSuperview().offset(-offset)
+                make.bottom.equalTo(self.view.safeAreaLayoutGuide).offset(bottomOffset)
+                make.left.equalToSuperview().offset(offset)
+            }
+            button.layer.cornerRadius = radius
+            UIView.animate(withDuration: 0.2, animations: {
                 self.view.layoutIfNeeded()
             })
         }
