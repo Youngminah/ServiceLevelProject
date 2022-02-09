a6c7ec692dcd20611a51d10ed09d228bbffe4ab5
Youngminah <bluebelsmudi@gmail.com>
2022-02-09 23:17:08 +0900

    feat: UI구성

--- ServiceLevelProject/Presentation/TabBar/Home/View/View/Cell/SelectedHobbyCell.swift
+++ ServiceLevelProject/Presentation/TabBar/Home/View/View/Cell/SelectedHobbyCell.swift
@@ -11,11 +11,14 @@ class SelectedHobbyCell: UICollectionViewCell {
 
     static let identifier = "SelectedHobbyCell"
 
-    private let hobbyButton = UIButton()
+    private let borderView = UIView()
+    private let hobbyLabel = DefaultLabel(font: .title4R14, textColor: .green)
+    private let cancelButton = UIButton()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         setConstraint()
+        setConfiguration()
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -23,13 +26,35 @@ class SelectedHobbyCell: UICollectionViewCell {
     }
 
     private func setConstraint() {
-        addSubview(hobbyButton)
-        hobbyButton.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+        contentView.addSubview(borderView)
+        borderView.addSubview(cancelButton)
+        borderView.addSubview(hobbyLabel)
+        borderView.snp.makeConstraints { make in
+            make.top.bottom.left.equalToSuperview()
+            make.right.equalToSuperview().priority(999)
+        }
+        cancelButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.right.equalToSuperview().offset(-16)
+            make.width.height.equalTo(16)
+        }
+        hobbyLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(5)
+            make.left.equalToSuperview().offset(16)
+            make.bottom.equalToSuperview().offset(-5)
+            make.right.equalTo(cancelButton.snp.left).offset(-6)
         }
     }
 
+    private func setConfiguration() {
+        borderView.layer.borderWidth = 1
+        borderView.layer.borderColor = UIColor.green.cgColor
+        borderView.layer.cornerRadius = 8
+        cancelButton.setImage(Asset.xmark.image, for: .normal)
+        cancelButton.tintColor = .green
+    }
+
     func updateUI(hobbyInfo: Hobby) {
-        hobbyButton.setTitle(hobbyInfo.content, for: .normal)
+        hobbyLabel.text = hobbyInfo.content
     }
 }
