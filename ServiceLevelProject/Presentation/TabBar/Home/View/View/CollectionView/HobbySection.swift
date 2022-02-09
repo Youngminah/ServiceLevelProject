74d269925d96c71ec41c12cf6eaf107c9943b926
Youngminah <bluebelsmudi@gmail.com>
2022-02-10 01:10:17 +0900

    feat: section enum

--- ServiceLevelProject/Presentation/TabBar/Home/View/View/CollectionView/HobbySection.swift
+++ ServiceLevelProject/Presentation/TabBar/Home/View/View/CollectionView/HobbySection.swift
@@ -11,9 +11,25 @@ import RxDataSources
 
 typealias HobbySectionModel = SectionModel<HobbySection, HobbyItem>
 
-enum HobbySection: Equatable {
+enum HobbySection: Int, Equatable {
     case near
     case selected
+
+    init(index: Int) {
+        switch index {
+        case 0: self = .near
+        default: self = .selected
+        }
+    }
+
+    var headerTitle: String {
+        switch self {
+        case .near:
+            return "지금 주변에는"
+        case .selected:
+            return "내가 하고 싶은"
+        }
+    }
 }
 
 enum HobbyItem: Equatable {
