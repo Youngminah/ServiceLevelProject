a4762ed5cd30fc6aa384d7da027d6dde18d328e8
Youngminah <bluebelsmudi@gmail.com>
2022-02-11 19:18:49 +0900

    feat: 새싹찾기 바디 모델

---
+++ ServiceLevelProject/Domain/Model/Request/SearchSesacQuery.swift
@@ -0,0 +1,15 @@
+//
+//  SearchSesac.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/02/11.
+//
+
+import Foundation
+
+struct SearchSesacQuery {
+    
+    let type: GenderCase
+    let coordinate: Coordinate
+    let hobbys: [String]
+}
