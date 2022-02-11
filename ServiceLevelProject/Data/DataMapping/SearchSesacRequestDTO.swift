a621d94c5e9ce7a6c4377fd317c666ec173b4fea
Youngminah <bluebelsmudi@gmail.com>
2022-02-11 19:18:35 +0900

    feat: 새싹찾기 DTO

---
+++ ServiceLevelProject/Data/DataMapping/SearchSesacRequestDTO.swift
@@ -0,0 +1,40 @@
+//
+//  SearchSesacRequestDTO.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/02/11.
+//
+
+import Foundation
+
+struct SearchSesacRequestDTO: Codable {
+
+    var toDictionary: [String: Any] {
+        let dict: [String: Any] = [
+            "type": type,
+            "region": region,
+            "lat": lat,
+            "long": long,
+            "hobbys": hobbys as NSArray
+        ]
+        return dict
+    }
+
+    var region: Int {
+        let computedLat = Int((lat + 90.0) * 100)
+        let computedLng = Int((long + 180.0) * 100)
+        let computedTotal = "\(computedLat)\(computedLng)" as NSString
+        return computedTotal.integerValue
+    }
+    let type: Int
+    let long: Double
+    let lat: Double
+    let hobbys: [String]
+
+    init(searchSesac: SearchSesacQuery) {
+        self.type = searchSesac.type.value
+        self.long = searchSesac.coordinate.longitude
+        self.lat = searchSesac.coordinate.latitude
+        self.hobbys = searchSesac.hobbys
+    }
+}
