2580abc58b7c95bf0177c31bec0f2bfbf320dab6
Youngminah <bluebelsmudi@gmail.com>
2022-02-11 19:19:53 +0900

    feat: 취미찾기 유즈케이스

---
+++ ServiceLevelProject/Domain/UseCase/HobbySearchUseCase.swift
@@ -0,0 +1,73 @@
+//
+//  HobbySearchUseCase.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/02/11.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+
+final class HobbySearchUseCase {
+
+    private let userRepository: UserRepositoryType
+    private let fireBaseRepository: FirbaseRepositoryType
+    private let sesacRepository: SesacRepositoryType
+
+    var successOnqueueSignal = PublishRelay<Onqueue>()
+    var unKnownErrorSignal = PublishRelay<Void>()
+
+    init(
+        userRepository: UserRepositoryType,
+        fireBaseRepository: FirbaseRepositoryType,
+        sesacRepository: SesacRepositoryType
+    ) {
+        self.userRepository = userRepository
+        self.fireBaseRepository = fireBaseRepository
+        self.sesacRepository = sesacRepository
+    }
+
+    func requestOnqueue(coordinate: Coordinate) {
+        self.sesacRepository.requestOnqueue(userLocationInfo: coordinate) { [weak self] response in
+            guard let self = self else { return }
+            switch response {
+            case .success(let onqueue):
+                self.successOnqueueSignal.accept(onqueue)
+            case .failure(let error):
+                switch error {
+                case .inValidIDTokenError:
+                    self.requestIDToken {
+                        self.requestOnqueue(coordinate: coordinate)
+                    }
+                default:
+                    self.unKnownErrorSignal.accept(())
+                }
+            }
+        }
+    }
+
+    private func requestIDToken(completion: @escaping () -> Void) {
+        fireBaseRepository.requestIdtoken { [weak self] response in
+            guard let self = self else { return }
+            switch response {
+            case .success(let idToken):
+                print("재발급 성공--> \(idToken)")
+                self.saveIdTokenInfo(idToken: idToken)
+                completion()
+            case .failure(let error):
+                print(error.description)
+                self.logoutUserInfo()
+                self.unKnownErrorSignal.accept(())
+            }
+        }
+    }
+
+    private func saveIdTokenInfo(idToken: String) {
+        self.userRepository.saveIdTokenInfo(idToken: idToken)
+    }
+
+    private func logoutUserInfo() {
+        self.userRepository.logoutUserInfo()
+    }
+}
