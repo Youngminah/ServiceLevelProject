a1ef7f979f1bad0d40c6175099f8fd6737c6a652
Youngminah <bluebelsmudi@gmail.com>
2022-02-16 17:05:09 +0900

    feat: 내 매칭상태 모델

---
+++ ServiceLevelProject/Domain/Model/Response/MyQueueState.swift
@@ -0,0 +1,17 @@
+//
+//  MyQueueState.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/02/15.
+//
+
+import Foundation
+
+struct MyQueueState: Equatable {
+
+    let dodged: Int
+    let matched: Int
+    let reviewed: Int
+    let matchedNick: String
+    let matchedUid: String
+}
