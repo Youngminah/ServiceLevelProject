eef9db74acb2536ac2f39caa8e90307a450ebd0b
Youngminah <bluebelsmudi@gmail.com>
2022-02-17 22:44:18 +0900

    feat: 리뷰 요청 바디

---
+++ ServiceLevelProject/Domain/Model/Request/ReviewQuery.swift
@@ -0,0 +1,15 @@
+//
+//  ReviewQuery.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/02/17.
+//
+
+import Foundation
+
+struct ReviewQuery {
+
+    let matchedUserID: String
+    let reputation: [Int]
+    let text: String
+}
