d2f20d2bc94057ee8ad03ea2b4a3a70ac907ed38
Youngminah <bluebelsmudi@gmail.com>
2022-02-17 16:35:35 +0900

    feat: 채팅 유즈케이스

--- ServiceLevelProject/Domain/UseCase/ChatUseCase.swift
+++ ServiceLevelProject/Domain/UseCase/ChatUseCase.swift
@@ -8,25 +8,35 @@
 import Foundation
 import RxSwift
 import RxRelay
+import SocketIO
 
 final class ChatUseCase {
 
     private let userRepository: UserRepositoryType
     private let fireBaseRepository: FirbaseRepositoryType
     private let sesacRepository: SesacRepositoryType
+    private let socketIORepository: SocketIORepositoryType
 
-    private let successRequestMyQueueState = PublishRelay<MyQueueState>()
-    var unKnownErrorSignal = PublishRelay<Void>()
+    let successRequestMyQueueState = PublishRelay<MyQueueState>()
+    let receivedChat = PublishRelay<Chat>()
+    let sendChat = PublishRelay<Chat>()
+    let unKnownErrorSignal = PublishRelay<Void>()
 
     init(
         userRepository: UserRepositoryType,
         fireBaseRepository: FirbaseRepositoryType,
-        sesacRepository: SesacRepositoryType
+        sesacRepository: SesacRepositoryType,
+        socketIORepository: SocketIORepositoryType
     ) {
         self.userRepository = userRepository
         self.fireBaseRepository = fireBaseRepository
         self.sesacRepository = sesacRepository
+        self.socketIORepository = socketIORepository
     }
+}
+
+// MARK: - 서버 레포
+extension ChatUseCase {
 
     func requestMyQueueState() {
         self.sesacRepository.requestMyQueueState { [weak self] response in
@@ -34,6 +44,8 @@ final class ChatUseCase {
             switch response {
             case .success(let myQueue):
                 self.successRequestMyQueueState.accept(myQueue)
+                self.saveMatchedUserIDInfo(id: myQueue.matchedUid)
+                self.connectSocket()
             case .failure(let error):
                 switch error {
                 case .inValidIDTokenError:
@@ -47,20 +59,98 @@ final class ChatUseCase {
         }
     }
 
-    private func requestIDToken(completion: @escaping () -> Void) {
-        fireBaseRepository.requestIdtoken { [weak self] response in
+    func requestSendChat(chatQuery: ChatQuery) {
+        let id = self.userRepository.fetchMatchedUserIDInfo()!
+        self.sesacRepository.requestSendChat(to: id, chatQuery: chatQuery)  { [weak self] response in
             guard let self = self else { return }
             switch response {
-            case .success(let idToken):
-                print("재발급 성공--> \(idToken)")
-                self.saveIdTokenInfo(idToken: idToken)
-                completion()
+            case .success(let chat):
+                self.sendChat.accept(chat)
             case .failure(let error):
-                print(error.description)
-                self.logoutUserInfo()
-                self.unKnownErrorSignal.accept(())
+                switch error {
+                case .inValidIDTokenError:
+                    self.requestIDToken {
+                        self.requestMyQueueState()
+                    }
+                default:
+                    self.unKnownErrorSignal.accept(())
+                }
             }
         }
+    }
+    //
+    //    func requestChat(to id: String, dateString: String) {
+    //        self.sesacRepository.requestChat(to: id, dateString: dateString) { [weak self] response in
+    //            guard let self = self else { return }
+    //            switch response {
+    //            case .success(let chatList):
+    //                self.successChat.accept(chatList)
+    //            case .failure(let error):
+    //                switch error {
+    //                case .inValidIDTokenError:
+    //                    self.requestIDToken {
+    //                        self.requestMyQueueState()
+    //                    }
+    //                default:
+    //                    self.unKnownErrorSignal.accept(())
+    //                }
+    //            }
+    //        }
+    //    }
+
+        private func requestIDToken(completion: @escaping () -> Void) {
+            fireBaseRepository.requestIdtoken { [weak self] response in
+                guard let self = self else { return }
+                switch response {
+                case .success(let idToken):
+                    print("재발급 성공--> \(idToken)")
+                    self.saveIdTokenInfo(idToken: idToken)
+                    completion()
+                case .failure(let error):
+                    print(error.description)
+                    self.logoutUserInfo()
+                    self.unKnownErrorSignal.accept(())
+                }
+            }
+        }
+}
+
+// MARK: - 소켓 통신 레포
+extension ChatUseCase {
+
+    func socketChatInfo() {
+        self.socketIORepository.receivedChat { [weak self] chat, ack in
+            print("누군가에게 메세지 도착!!-->", chat)
+            self?.receivedChat.accept(chat)
+        }
+    }
+
+    func connectSocket() {
+        self.socketIORepository.connectSocket()
+    }
+
+    func disconnectSocket() {
+        self.socketIORepository.disconnectSocket()
+    }
+
+    func listenConnect() {
+        self.socketIORepository.listenConnect { data, ack in
+            print("SOCKET IS CONNECTED: ", data, ack)
+        }
+    }
+
+    func listenDisconnect() {
+        self.socketIORepository.listenDisconnect { data, ack in
+            print("SOCKET IS DISCONNECTED: ", data, ack)
+        }
+    }
+}
+
+// MARK: - 유저 디폴트 레포
+extension ChatUseCase {
+
+    private func saveMatchedUserIDInfo(id: String) {
+        self.userRepository.saveMatchedUserIDInfo(id: id)
     }
 
     private func saveIdTokenInfo(idToken: String) {
