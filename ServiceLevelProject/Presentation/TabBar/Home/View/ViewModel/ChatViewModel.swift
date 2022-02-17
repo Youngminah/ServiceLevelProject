b101d99b86bb0508ef279417d30331b244a5c802
Youngminah <bluebelsmudi@gmail.com>
2022-02-17 16:33:21 +0900

    feat: 채팅받기 보내기 로직 추가 뷰모델

--- ServiceLevelProject/Presentation/TabBar/Home/View/ViewModel/ChatViewModel.swift
+++ ServiceLevelProject/Presentation/TabBar/Home/View/ViewModel/ChatViewModel.swift
@@ -16,17 +16,21 @@ final class ChatViewModel: ViewModelType {
 
     struct Input {
         let viewDidLoad: Observable<Void>
+        let viewDidDisappear: Signal<Void>
         let backBarButtonTap: Signal<Void>
         let detailBarButtonTap: Signal<Void>
+        let sendChat: Signal<String>
     }
     struct Output {
         let showToastAction: Signal<String>
+        let myState: Signal<MyQueueState>
         let chatList: Driver<[Chat]>
     }
     var disposeBag = DisposeBag()
 
     private let chatList = BehaviorRelay<[Chat]>(value: [])
     private let showToastAction = PublishRelay<String>()
+    private let myState = PublishRelay<MyQueueState>()
 
     init(coordinator: HomeCoordinator?, useCase: ChatUseCase) {
         self.coordinator = coordinator
@@ -36,12 +40,26 @@ final class ChatViewModel: ViewModelType {
     func transform(input: Input) -> Output {
         input.viewDidLoad
             .subscribe(onNext: { [weak self] in
+                self?.useCase.socketChatInfo()
+                self?.useCase.requestMyQueueState()
                 self?.chatList.accept([
                     Chat(_id: "me", __v: 0, text: "안녕하세요 :)", createdAt: "2022-01-16T06:55:54.784Z".toDate, from: "NuK12cdVaDVcc9e4ctxsLMNCrHQ2", to: "me"),
                     Chat(_id: "me", __v: 0, text: "안녕하세요!! 자전거 타러가시나여 이번주에 시간되시나여?", createdAt: "2022-01-16T06:55:54.784Z".toDate, from: "NuK12cdVaDVcc9e4ctxsLMNCrHQ2", to: "me"),
                     Chat(_id: "me", __v: 0, text: "안녕하세요!!", createdAt: "2022-01-16T06:55:55.784Z".toDate, from: "me", to: "NuK12cdVaDVcc9e4ctxsLMNCrHQ2"),
                     Chat(_id: "me", __v: 0, text: "포근포근 달콤해\n둥글둥글 부푸는 마음\n맛있는꿈을 그려봐요\n행복한 꿈빛 파티시엘~~~~~", createdAt: "2022-01-16T06:55:58.784Z".toDate, from: "me", to: "NuK12cdVaDVcc9e4ctxsLMNCrHQ2")
                 ])
+            })
+            .disposed(by: disposeBag)
+
+        input.sendChat
+            .emit(onNext: { [weak self] text in
+                self?.useCase.requestSendChat(chatQuery: ChatQuery(text: text))
+            })
+            .disposed(by: disposeBag)
+
+        input.viewDidDisappear
+            .emit(onNext: { [weak self] in
+                self?.useCase.disconnectSocket()
             })
             .disposed(by: disposeBag)
 
@@ -57,8 +75,37 @@ final class ChatViewModel: ViewModelType {
             })
             .disposed(by: disposeBag)
 
+        self.useCase.successRequestMyQueueState
+            .asSignal()
+            .emit(onNext: { [weak self] state in
+                self?.myState.accept(state)
+            })
+            .disposed(by: disposeBag)
+
+        self.useCase.sendChat
+            .asSignal()
+            .emit(onNext: { [weak self] chat in
+                guard let self = self else { return }
+                var list = self.chatList.value
+                list.append(chat)
+                //print(chat)
+                self.chatList.accept(list)
+            })
+            .disposed(by: disposeBag)
+
+        self.useCase.receivedChat
+            .asSignal()
+            .emit(onNext: { [weak self] chat in
+                guard let self = self else { return }
+                var list = self.chatList.value
+                list.append(chat)
+                self.chatList.accept(list)
+            })
+            .disposed(by: disposeBag)
+
         return Output(
             showToastAction: showToastAction.asSignal(),
+            myState: myState.asSignal(),
             chatList: chatList.asDriver()
         )
     }
