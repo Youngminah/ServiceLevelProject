79311b85ac7ffe687700c4a7b95a68d9c9ca0984
Youngminah <bluebelsmudi@gmail.com>
2022-02-20 22:44:44 +0900

    feat: 새싹 셀, 배경 셀

--- ServiceLevelProject/Presentation/TabBar/SesacShop/View/Cell/BackgroundCell.swift
+++ ServiceLevelProject/Presentation/TabBar/SesacShop/View/Cell/BackgroundCell.swift
@@ -14,22 +14,66 @@ final class BackgroundCell: BaseTableViewCell {
     private let sesacImageView = UIImageView()
     private let titleLabel = DefaultLabel(font: .title2R16, textColor: .black)
     private let contentLabel = DefaultLabel(font: .body3R14, textColor: .black)
-    private let priceLabel = PaddingLabel()
+    private let priceLabel = DefaultLabel(font: .title5M12, textColor: .white)
 
     override func setView() {
         super.setView()
         contentView.addSubview(sesacImageView)
+        contentView.addSubview(priceLabel)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(contentLabel)
     }
 
     override func setConstraints() {
         super.setConstraints()
         sesacImageView.snp.makeConstraints { make in
-            make.left.top.equalToSuperview().offset(16)
-            make.height.width.equalToSuperview().multipliedBy(165.0 * (UIScreen.main.bounds.width - 32))
+            make.top.equalToSuperview().offset(8)
+            make.left.equalToSuperview().offset(16)
+            make.bottom.equalToSuperview().offset(-8)
+            make.width.equalTo(sesacImageView.snp.height)
+        }
+        priceLabel.snp.makeConstraints { make in
+            make.right.equalToSuperview().offset(-16)
+            make.centerY.equalToSuperview().multipliedBy(0.8)
+            make.width.equalTo(52)
+            make.height.equalTo(20)
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(priceLabel)
+            make.left.equalTo(sesacImageView.snp.right).offset(16)
+            make.right.equalTo(priceLabel.snp.left).offset(-8)
+            make.height.equalTo(20)
+        }
+        contentLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.left.equalTo(sesacImageView.snp.right).offset(16)
+            make.right.equalToSuperview().offset(-16)
         }
     }
 
     override func setConfiguration() {
         super.setConfiguration()
+        sesacImageView.contentMode = .scaleAspectFill
+        sesacImageView.layer.cornerRadius = 8
+        sesacImageView.layer.masksToBounds = true
+        priceLabel.backgroundColor = .green
+        priceLabel.layer.cornerRadius = 8
+        priceLabel.layer.masksToBounds = true
+        titleLabel.textAlignment = .left
+        contentLabel.textAlignment = .natural
+    }
+
+    func updateUI(background: SesacBackgroundCase) {
+        sesacImageView.image = background.image
+        titleLabel.text = background.name
+        contentLabel.text = background.content
+        priceLabel.text = background.price
+        if background.price == "보유" {
+            priceLabel.backgroundColor = .gray2
+            priceLabel.textColor = .gray7
+        } else {
+            priceLabel.backgroundColor = .green
+            priceLabel.textColor = .white
+        }
     }
 }
--- ServiceLevelProject/Presentation/TabBar/SesacShop/View/Cell/SesacCell.swift
+++ ServiceLevelProject/Presentation/TabBar/SesacShop/View/Cell/SesacCell.swift
@@ -14,7 +14,7 @@ final class SesacCell: UICollectionViewCell {
     private let imageView = UIImageView()
     private let titleLabel = DefaultLabel(font: .title2R16, textColor: .black)
     private let contentLabel = DefaultLabel(font: .body3R14, textColor: .black)
-    private let priceLabel = PaddingLabel()
+    private let priceLabel = DefaultLabel(font: .title5M12, textColor: .white)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -28,9 +28,30 @@ final class SesacCell: UICollectionViewCell {
 
     private func setConstraint() {
         contentView.addSubview(imageView)
+        contentView.addSubview(priceLabel)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(contentLabel)
         imageView.snp.makeConstraints { make in
-            make.top.left.right.equalToSuperview()
+            make.top.equalToSuperview()
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
             make.height.equalTo(imageView.snp.width)
+        }
+        priceLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(12)
+            make.right.equalToSuperview()
+            make.width.equalTo(52)
+            make.height.equalTo(20)
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(8)
+            make.left.equalToSuperview()
+            make.right.equalTo(priceLabel.snp.left).offset(8)
+            make.height.equalTo(26)
+        }
+        contentLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom)
+            make.left.bottom.right.equalToSuperview()
         }
     }
 
@@ -38,5 +59,21 @@ final class SesacCell: UICollectionViewCell {
         imageView.layer.borderWidth = 1
         imageView.layer.borderColor = UIColor.gray4.cgColor
         imageView.layer.cornerRadius = 8
+        priceLabel.backgroundColor = .green
+        priceLabel.layer.cornerRadius = 8
+        priceLabel.layer.masksToBounds = true
+        titleLabel.textAlignment = .left
+        contentLabel.textAlignment = .natural
+    }
+
+    func updateUI(sesac: SesacImageCase) {
+        imageView.image = sesac.image
+        titleLabel.text = sesac.name
+        contentLabel.text = sesac.content
+        priceLabel.text = sesac.price
+        if sesac.price == "보유" {
+            priceLabel.backgroundColor = .gray2
+            priceLabel.textColor = .gray7
+        }
     }
 }
