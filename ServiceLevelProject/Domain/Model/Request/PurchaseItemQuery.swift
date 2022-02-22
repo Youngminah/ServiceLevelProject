ab634f3fd71fded7a344f26b92cebbc2424b6b5a
Youngminah <bluebelsmudi@gmail.com>
2022-02-22 15:55:27 +0900

    feat: 구매 요청 영수증확인 쿼리

---
+++ ServiceLevelProject/Domain/Model/Request/PurchaseItemQuery.swift
@@ -0,0 +1,14 @@
+//
+//  PurchaseItemQuery.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/02/22.
+//
+
+import Foundation
+
+struct PurchaseItemQuery {
+
+    let receipt: String
+    let product: String
+}
