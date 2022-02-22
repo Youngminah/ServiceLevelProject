b06be71381466c2b906e6ed9d2145a14f9ded763
Youngminah <bluebelsmudi@gmail.com>
2022-02-22 15:53:44 +0900

    feat: 새싹, 배경 저장 업뎃 쿼리

---
+++ ServiceLevelProject/Domain/Model/Request/UpdateShopQuery.swift
@@ -0,0 +1,14 @@
+//
+//  UpdateShopQuery.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/02/21.
+//
+
+import Foundation
+
+struct UpdateShopQuery {
+
+    let sesac: SesacImageCase
+    let background: SesacBackgroundCase
+}
