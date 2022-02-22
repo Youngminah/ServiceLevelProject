684d1e465ada7d96278a4a27fe3c257ce8ab94a7
Youngminah <bluebelsmudi@gmail.com>
2022-02-22 16:04:01 +0900

    feat: 새싹샵 새싹, 배경 프로토콜 델리게이트

---
+++ ServiceLevelProject/Presentation/TabBar/SesacShop/Protocol/PreviewSesacDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  PreviewSesacDelegate.swift
+//  ServiceLevelProject
+//
+//  Created by meng on 2022/02/21.
+//
+
+import Foundation
+
+protocol PreviewSesacDeleagete: AnyObject {
+
+    func updateSesac(sesac: SesacImageCase)
+
+    func updateBackground(background: SesacBackgroundCase)
+}
