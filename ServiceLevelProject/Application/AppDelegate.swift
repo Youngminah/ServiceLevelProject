d7a60f68ddcb859c699a82db73fec787ea6c62d0
Youngminah <bluebelsmudi@gmail.com>
2022-02-22 20:35:51 +0900

    fix: 아이폰 10.0미만 대응 코드 삭제

--- ServiceLevelProject/Application/AppDelegate.swift
+++ ServiceLevelProject/Application/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 import FirebaseMessaging
+
 import UserNotifications
 
 @main
@@ -18,20 +19,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         AppAppearance.setupAppearance()
         FirebaseApp.configure()
 
-        if #available(iOS 10.0, *) {
-          // For iOS 10 display notification (sent via APNS)
-          UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().delegate = self
 
-          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-          UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: { _, _ in }
-          )
-        } else {
-          let settings: UIUserNotificationSettings =
-            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-          application.registerUserNotificationSettings(settings)
-        }
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: { _, _ in }
+        )
         application.registerForRemoteNotifications()
         getCurrentFCMToken()
         return true
